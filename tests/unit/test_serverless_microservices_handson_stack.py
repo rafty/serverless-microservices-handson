12d3973ccf3462c08930af9619f9f5ef8cdf4387
takashi <yagita.takashi@gmail.com>
2022-11-25 21:07:10 +0900

    Initial commit

---
+++ tests/unit/test_serverless_microservices_handson_stack.py
@@ -0,0 +1,15 @@
+import aws_cdk as core
+import aws_cdk.assertions as assertions
+
+from serverless_microservices_handson.serverless_microservices_handson_stack import ServerlessMicroservicesHandsonStack
+
+# example tests. To run these tests, uncomment this file along with the example
+# resource in serverless_microservices_handson/serverless_microservices_handson_stack.py
+def test_sqs_queue_created():
+    app = core.App()
+    stack = ServerlessMicroservicesHandsonStack(app, "serverless-microservices-handson")
+    template = assertions.Template.from_stack(stack)
+
+#     template.has_resource_properties("AWS::SQS::Queue", {
+#         "VisibilityTimeout": 300
+#     })
